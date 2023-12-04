d4f674efefc2f3d471c27bd9f772df1522c8f1cd
Burnsypet <aburnoffice@gmail.com>
2023-12-04 07:14:04 +0000

    Handy file to get the input automatically

---
+++ get_input.py
@@ -0,0 +1,17 @@
+import argparse
+
+import requests
+
+a_parser = argparse.ArgumentParser()
+a_parser.add_argument('day', type=int)
+a_parser.add_argument('session', type=str)
+args = a_parser.parse_args()
+
+with open(args.session, 'r') as session_file:
+    session = session_file.read()
+
+url = f'https://adventofcode.com/2023/day/{args.day}/input'
+content = requests.get(url, cookies={'session': session})
+
+with open('puzzle_input', 'w') as file:
+    file.write(content.text)
